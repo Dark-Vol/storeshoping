6f8cbb3481d60b0d284c5ec1f38aefad4cb18218
Dark-Vol <overlord28985@gmail.com>
2025-01-29 20:46:41 +0200

    add header

---
+++ client/src/components/headers/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Контакти</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Про нас</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
