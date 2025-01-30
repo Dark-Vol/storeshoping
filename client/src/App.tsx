0836404707f62286426afe7142457475a44b58d4
Dark-Vol <overlord28985@gmail.com>
2025-01-30 13:13:23 +0200

    add Suspense, React.lazy and page error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Navigate from './components/headers/Navigation';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import MainLoader from "./components/MainLoader"
 
+
+const ErrorPage = lazy(
+  () => import(/* webpackChunkName: "ErrorPage"*/ "./pages/error")
+);
 const App: React.FC = () => {
   return (
     <>
       <Navigate />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Suspense fallback={<MainLoader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </>
   )
 };
