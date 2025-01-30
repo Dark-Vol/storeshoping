dfd6b46e52b62f7b1e72bc5f104320f0eb21b993
Dark-Vol <overlord28985@gmail.com>
2025-01-30 13:07:17 +0200

    add for processing asynchronous operations

---
+++ client/src/components/MainLoader/index.tsx
@@ -0,0 +1,11 @@
+const MainLoader: React.FC = () => {
+    return (
+        <div className="main-loader">
+            <div className="main-loader__container">
+                <div className="main-loader__container-inner"></div>
+            </div>
+        </div>
+    );
+};
+
+export default MainLoader;
